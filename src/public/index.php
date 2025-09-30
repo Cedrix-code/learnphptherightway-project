6583473df741b4ec0230ffa54dbfc0ddf313c03f
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-09-30 15:03:30 +0800

    Docker Tutorial - PHP The Right Way

--- src/public/index.php
+++ src/public/index.php
@@ -2,4 +2,13 @@
 
 declare(strict_types = 1);
 
-phpinfo();
+require_once '../Transaction.php';
+
+$class = 'Transaction';
+
+$amount = (new $class(100, 'Transaction 1'))
+    ->addTax(8)
+    ->applyDiscount(10)
+    ->getAmount();
+
+var_dump($amount);
