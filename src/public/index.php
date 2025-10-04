7003f4a0ac3b35a898ceefd3c99f285cfcc52cbf
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-10-04 23:54:41 +0800

    Save current work on branch 2.1 --Class Constants

--- src/public/index.php
+++ src/public/index.php
@@ -1,13 +1,12 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
+use App\Enums\Status;
 use App\PaymentGateway\Paddle\Transaction;
 
-$paddleTransaction = new Transaction();
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$id = new \Ramsey\Uuid\UuidFactory();
+$transaction = new Transaction();
 
-echo $id->uuid4();
+$transaction->setStatus(Status::PAID);
 
-var_dump($paddleTransaction);
+var_dump($transaction);
